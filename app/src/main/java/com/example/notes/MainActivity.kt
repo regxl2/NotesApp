92dc01298dbc4defc3b863263ecef2f4ea6cfa91
Abhishek Rathore <abhishekrathore@regxl-2.local>
2024-07-20 01:06:30 +0530

    added authentication

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.notes
 
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
